a512adad4a777431b8dd61baeb8ae09582271b51
Agustin Rueda <agustinrueda06@gmail.com>
2026-08-26 06:41:31 -0300

    actualizar dependencias y mejorar configuración de entorno

--- dependencies.js
+++ dependencies.js
@@ -2,7 +2,7 @@ import { addDependency } from "./dependency.js";
 import { UserServices } from "./services/user_services.js";
 import { LoginServices } from "./services/login_services.js";
 import { SessionsServices } from "./services/sessions_services.js";
-import { ProductService } from "./services/product_service.js"; // <-- 1. IMPORTAR
+import { ProductService } from "./services/product_services.js"; // <-- 1. IMPORTAR
 
 import UserMongo from "./mongo-db/user_mongo.js";
 import SessionMongo from "./mongo-db/sessions_mongo.js";
